fcd436f42ca33885583b2ea9e1262c86924b160f
Konstantion <yagudinkostya@gmail.com>
2025-05-03 12:12:55 +0300

    feat: add question page route

--- playground-frontend/src/rout/Routes.jsx
+++ playground-frontend/src/rout/Routes.jsx
@@ -1,9 +1,16 @@
 import LoginPage from '../pages/LoginPage.jsx';
 import HomePage from '../pages/HomePage.jsx';
+import QuestionDetailPage from '@/pages/QuestionDetailPage.jsx';
 
 export const RHome = '/home';
+export const RQuestions = '/questions';
 
 export const Routes = Object.freeze({
     Login: { path: '/login', element: <LoginPage />, public: true },
     Home: { path: `${RHome}/:section`, element: <HomePage />, public: false }, // updated path
+    QuestionDetail: {
+        path: `${RQuestions}/:id`,
+        element: <QuestionDetailPage />,
+        public: false,
+    },
 });
